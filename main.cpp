6c747d6f79e79bfefc2ea9328a25424cba46af70
Alexandr Axentev <vio.aksenteval@gmail.com>
2024-09-05 10:36:12 +0300

    Add functions: getCountStrWithUpperOrLowerCase isUpperOrLowerA

--- main.cpp
+++ main.cpp
@@ -4,12 +4,26 @@
 
 using namespace std;
 
-// Get count elements in <vector> is_nervous equals true
-void PrintWorryCount(const vector<bool>& is_nervous) {
+
+bool isUpperOrLowerA(string str) {
+    if (str[0] == 'A' or str[0] == 'a') {
+        return true;
+    }
+    return false;
+}
+
+// Get count elements in vector "is_nervous" equals true
+void PrintWorryCount(const vector<bool> &is_nervous) {
     cout << count(is_nervous.begin(), is_nervous.end(), 1) << endl;
+}
+
+// Get count string in vector "str" which starting with uppercase or lowercase "a"
+int getCountStrWithUpperOrLowerCase(vector<string> str) {
+    return count_if(str.begin(), str.end(), isUpperOrLowerA);
 }
 
 int main() {
     PrintWorryCount({true, true, false, true});
+    cout << getCountStrWithUpperOrLowerCase({"qwdas", "Asdsd"});
     return 0;
 }
