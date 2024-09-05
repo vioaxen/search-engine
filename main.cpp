24e8f2c98294c4b9a1fc71c93ac0c90caf773a5e
Alexandr Axentev <vio.aksenteval@gmail.com>
2024-09-05 10:18:30 +0300

    Add function: PrintWorryCount

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+// Get count elements in <vector> is_nervous equals true
+void PrintWorryCount(const vector<bool>& is_nervous) {
+
+    int worry_count = 0;
+    for (bool status : is_nervous) {
+        if (status) {
+            ++worry_count;
+        }
+    }
+    cout << worry_count << endl;
+}
+
+int main() {
+    PrintWorryCount({true, true, false, true});
     return 0;
 }
